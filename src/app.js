bfe6a1ec1ddd38cdd1c5a8c53ad275b42401b5f1
lupacco <pagottolc@gmail.com>
2023-01-07 22:01:38 -0300

    singup OK

--- src/app.js
+++ src/app.js
@@ -1,4 +1,67 @@
+import express, {json} from "express"
+import cors from "cors"
 
-const PORT = 5005
+const users = [
+    {
+        username: "bobesponja",
+        avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info"
+    }
+]
+
+const tweets = [
+    {
+        username: "bobesponja",
+        tweet: "eu amo o hub"
+    }
+]
+
+const app = express()
+
+app.use(cors())
+app.use(json())
+
+app.post("/sign-up", (req, res) => {
+    const user = req.body
+
+    if(!user.username || !user.avatar){
+        return res.status(401).send("Todos os campos são obrigatórios!")
+    }
+    users.push(req.body)
+    res.status(201).send('OK')
+
+    console.log(users)
+})
+
+app.get("/tweets", (req, res) => {
+    res.send(users[1])
+})
+
+app.post("/tweets", (req, res) => {
+    const tweet = req.body
+
+    const userIsRegistered = users.find(user => {user.username === tweet.username})
+    console.log(users[1])
+    if(!userIsRegistered){
+        return res.status(401).send("UNAUTHORAZED")
+    }
+
+    tweets.push(tweet)
+    res.status(201).send("OK")
+    console.log(tweets)
+})
+
+
+
+const PORT = 5000
 
 app.listen(PORT, () => {console.log(`SERVIDOR RODANDO NA PORTA: ${PORT}`)})
+
+
+// 200: Ok => Significa que deu tudo certo com a requisição
+// 201: Created => Sucesso na criação do recurso
+// 301: Moved Permanently => Significa que o recurso que você está tentando acessar foi movido pra outra URL
+// 401: Unauthorized => Significa que você não tem acesso a esse recurso
+// 404: Not Found => Significa que o recurso pedido não existe
+// 409: Conflict => Significa que o recurso que você está tentando inserir já foi inserido
+// 422: Unprocessable Entity => Significa que a requisição enviada não está no formato esperado
+// 500: Internal Server Error => Significa que ocorreu algum erro desconhecido no servidor
